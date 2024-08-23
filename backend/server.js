70c7580d0274b7165d90ea3a981efcb1e7219226
mayankjha2021 <mayankk.ug21.ee@nitp.ac.in>
2024-08-23 18:28:07 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.form);
+    res.send('Hello World!')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
